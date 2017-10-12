6ae6e41061fd1f8dacbcc215f4a09a1d8dcde693
Ranelle <ranelle.reyes@gmail.com>
2017-10-11 23:33:49 -0700

    Add framework for mocha testing

---
+++ Coding_Challenge1/test/test.js
@@ -0,0 +1,8 @@
+var assert = require('assert');
+describe('Testing', function() {
+  describe('Queue', function() {
+    it('should store an array once initialized', function() {
+
+    });
+  });
+});
