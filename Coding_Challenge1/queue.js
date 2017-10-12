b49c43f782029a234894723d9ad8f0091d63577b
Ranelle <ranelle.reyes@gmail.com>
2017-10-11 23:24:17 -0700

    Add planned functions for Queue and quick descriptions

--- Coding_Challenge1/queue.js
+++ Coding_Challenge1/queue.js
@@ -1,3 +1,26 @@
 class Queue {
-  
+  constructor(init_window) {
+    this.queue = [];
+  }
+
+  dequeue() {
+    // Dequeue
+  }
+
+  enqueue() {
+    // Enqueue
+  }
+
+  window_sum() {
+    // Calculates total of all subranges
+  }
+
+  subrange_sum() {
+    // Calculates how many increasing or decreasing subranges that start
+    // at the given queue element
+  }
+
+  update() {
+    // Updates subrange sums once something is enqueued
+  }
 }
