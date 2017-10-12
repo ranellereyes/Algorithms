9ce236f73c4f3dc1ab1938f3eeeebf07e90db61e
Ranelle <ranelle.reyes@gmail.com>
2017-10-11 23:43:06 -0700

    Add first wave of proposed Mocha tests

--- Coding_Challenge1/test/test.js
+++ Coding_Challenge1/test/test.js
@@ -2,7 +2,31 @@ var assert = require('assert');
 describe('Testing', function() {
   describe('Queue', function() {
     it('should store an array once initialized', function() {
+      assert.equal(true, false);
+    });
 
+    it('every element should be a POJO with value and current subsum', function() {
+      assert.equal(true, false);
+    });
+  });
+
+  describe('Initialization', function() {
+    describe('should correctly calculate subsums for the following cases:', function() {
+      it('all increasing', function() {
+        assert.equal(true, false);
+      });
+
+      it('all decreasing', function() {
+        assert.equal(true, false);
+      });
+
+      it('switches from increasing to decreasing', function() {
+        assert.equal(true, false);
+      });
+
+      it('handles 0\'s at the trailing edge correctly', function() {
+        assert.equal(true, false);
+      });
     });
   });
 });
