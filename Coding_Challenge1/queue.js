690a3a9c62191a09cc526c9ecad5f80ec76d5fec
Ranelle <ranelle.reyes@gmail.com>
2017-10-14 23:41:36 -0700

    Basic logic

--- Coding_Challenge1/queue.js
+++ Coding_Challenge1/queue.js
@@ -1,16 +1,22 @@
 export default class Queue {
   constructor(init_window) {
     this.queue = init_window.map(el => {
-      return { value: el, subrange_sum: 0 }; 
+      return { value: el, subrange_sum: 0 };
     });
+    this.current_sum = 0;
+    this.total = 0;
   }
 
   dequeue() {
     // Dequeue
+    let shifted = this.queue.shift;
+
+    this.total -= shifted.subrange_sum
   }
 
-  enqueue() {
+  enqueue(ele) {
     // Enqueue
+    this.queue.push({ value: ele, subrange_sum: current_sum })
   }
 
   window_sum() {
